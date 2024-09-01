e06468eb95af83a32bdc040a58c32fcff315f522
zyk1109 <1036389647@qq.com>
2024-09-01 11:50:54 +0800

    完成了Lab7

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -31,6 +31,16 @@ barrier()
   // then increment bstate.round.
   //
   
+  
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  if(++bstate.nthread == nthread){
+    bstate.nthread = 0;
+    bstate.round++;
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  } else {
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  }
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
--- notxv6/ph.c
+++ notxv6/ph.c
@@ -17,6 +17,9 @@ struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
+pthread_mutex_t locks[NBUCKET]; // 构建互斥锁
+
+
 double
 now()
 {
@@ -51,7 +54,10 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
+    pthread_mutex_lock(&locks[i]); // acquire lock
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&locks[i]); // release lock
+
   }
 }
 
@@ -115,6 +121,9 @@ main(int argc, char *argv[])
     keys[i] = random();
   }
 
+  for(int i = 0; i < NBUCKET; i++) {
+    pthread_mutex_init(&locks[i], NULL);
+  }
   //
   // first the puts
   //
